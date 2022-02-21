0cf8d2f19f4c77f5a43bf81792aecc9fd77ee2ef
Alexander V. Volokhov <antinitrino@yandex.ru>
2022-02-21 07:48:59 +0000

    fix_3

---
+++ variables.tf
@@ -0,0 +1,38 @@
+variable "token" {
+  description = "users token"
+  type        = string
+  sensitive   = true
+}
+
+variable "ubuntu20_04" {
+  description = "Image name"
+  type        = string
+  sensitive   = true
+}
+
+#variable "subnet" {
+#  description = "subnet"
+#  type        = string
+#  sensitive   = true
+#}
+
+
+
+variable "DevopsPracticumCloud" {
+  description = "Our Cloud"
+  type        = string
+  sensitive   = true
+}
+
+variable "PracticumFolder" {
+  description = "Our folder"
+  type        = string
+  sensitive   = true
+}
+
+variable "ZONA" {
+  description = "Our zona"
+  type        = string
+  sensitive   = true
+}
+
